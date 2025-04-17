b056e9f968075df2bea9c2b9255451294b70c452
Anni Järvenpää <anni.jarvenpaa@csc.fi>
2025-04-17 15:28:54 +0300

    underline importantness of internal work

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 while True:
     print("this feature is awesomest")
-    print("Internal work is important")
+    print("Internal work is importantest!!1!")
     print("External collaborators are the best!")
