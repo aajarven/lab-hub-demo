3aa7b1fc34e8a561d9f57a3ae9b73af9cb7fcbf3
Anni Järvenpää <anni.jarvenpaa@csc.fi>
2025-04-22 13:27:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 while True:
     print("this feature is awesomest")
     print("Internal work is fairly important")
-    print("I need Uni to review this!")
+    print("I don't need Uni to review this")
